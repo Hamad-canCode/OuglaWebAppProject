711105c679b7d93e8522d6f484222ded50ecd275
Muhammad Hamad <muhammadhamadwo@gmail.com>
2023-01-03 00:07:00 +0500

    Creating A Global Config Class to access common variables

--- OuglaWebApp/DataLogic/BlogHandling.cs
+++ OuglaWebApp/DataLogic/BlogHandling.cs
@@ -3,15 +3,16 @@ using OuglaWebApp.Models;
 
 namespace OuglaWebApp.DataLogic
 {
-    public class BlogHandling
+    public class BlogHandling:GlobalConfig
     {
-        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-M4962FO\MSSQLSERVER03;Initial Catalog=ougla;Integrated Security=True;TrustServerCertificate=true");
+
+        SqlConnection con = new SqlConnection(ConnectionString);
         public BlogHandling()
         {
 
         }
 
-        public void UploadBlog(BlogModel blog, string siteName)
+        public void UploadBlog(BlogModel blog)
         {
             con.Open();
             SqlCommand cmd = new SqlCommand($"INSERT INTO BlogsOf{siteName} (Title, Content,img) VALUES('{blog.Title}', '{blog.Content}',(Select BulkColumn FROM OPENROWSET(BULK N'C:\\Users\\lenovo\\Downloads\\Blank.png', SINGLE_BLOB)  as img));", con);
---
+++ OuglaWebApp/DataLogic/GlobalConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace OuglaWebApp.DataLogic
+{
+    public class GlobalConfig
+    {
+        public static string siteName;
+        public static string ConnectionString= @"Data Source=DESKTOP-M4962FO\MSSQLSERVER03;Initial Catalog=ougla;Integrated Security=True;TrustServerCertificate=true";
+        SqlConnection con = new SqlConnection(ConnectionString);
+        public GlobalConfig()
+        {
+
+        }
+    }
+}
