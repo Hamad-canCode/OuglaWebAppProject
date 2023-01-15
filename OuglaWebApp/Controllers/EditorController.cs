79d508ab600d05aab471f48067815cff4fdfadfa
Muhammad Hamad <muhammadhamadwo@gmail.com>
2023-01-15 05:52:10 +0500

    Made Update Methods for  Page Content (Not Tested)

--- OuglaWebApp/Controllers/EditorController.cs
+++ OuglaWebApp/Controllers/EditorController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OuglaWebApp.Models;
+using System.Reflection.Metadata;
 
 namespace OuglaWebApp.Controllers
 {
@@ -12,10 +13,54 @@ namespace OuglaWebApp.Controllers
         }
 
         [HttpPost]
-        public void EditorContent(Content content, IFormFile logo,IFormFile Banner, IFormFile objImg)
+        public async Task<IActionResult> EditorContent(Content content, IFormFile logo,IFormFile Banner, IFormFile objImg)
         {
-    
-        }
+            try
+            {
+                if (logo != null)
+                {
+                    using (var target = new MemoryStream())
+                    {
+                        await logo.CopyToAsync(target);
+                        content.logoImg = target.ToArray();
+                    }
+                }
+                if (Banner != null)
+                {
+                    using (var target = new MemoryStream())
+                    {
+                        await logo.CopyToAsync(target);
+                        content.logoImg = target.ToArray();
+                    }
+                }
+                if (objImg != null)
+                {
+                    using (var target = new MemoryStream())
+                    {
+                        await logo.CopyToAsync(target);
+                        content.logoImg = target.ToArray();
+                    }
+                }
 
+            }
+            catch (Exception)
+            {
+                TempData["msg"] = "<script>alert('Oops! Something went wrong');</script>";
+                return RedirectToAction("BlogEditor", "Blog");
+            }
+        }
+        public async Task<byte[]> ImgConversion(IFormFile file,string ImgType) 
+        {
+            byte[] imgInByte= null;
+            if (file != null)
+            {
+                using (var target = new MemoryStream())
+                {
+                    await file.CopyToAsync(target);
+                    imgInByte = target.ToArray();
+                }
+                return imgInByte;
+            }
+        }
     }
 }
--- OuglaWebApp/DataLogic/Editor.cs
+++ OuglaWebApp/DataLogic/Editor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using OuglaWebApp.Models;
+using System;
 using System.Reflection.Metadata;
 
 namespace OuglaWebApp.DataLogic
@@ -11,22 +12,51 @@ namespace OuglaWebApp.DataLogic
         public void UploadContent(Content content,string siteName)
         {
             con.Open();
-            //if (content.Image != null)
-            //{
-            //    string varBinary = ToVarbinary(content.Image);
-            //    //SqlCommand cmd = new SqlCommand($"INSERT INTO BlogsOf{siteName} (Title, Content,img) VALUES('{blog.Title}', '{blog.Content}',(Select BulkColumn FROM OPENROWSET(BULK N'C:\\Users\\lenovo\\Downloads\\Blank.png', SINGLE_BLOB)  as img));", con);
-            //    //  SqlCommand cmd = new SqlCommand($"INSERT INTO BlogsOf{siteName} (Title, Content,img) VALUES('{blog.Title}', '{blog.Content}',CONVERT(VARBINARY(MAX), '{blog.Image}'))", con);
-            //    SqlCommand cmd = new SqlCommand($"INSERT INTO BlogsOf{siteName} (Title, Content,img) VALUES('{content.Title}', '{content.Content}',{varBinary})", con);
+            if (true)
+            {
 
-            //    cmd.ExecuteNonQuery();
-            //}
-            //else
-            //{
-            //    SqlCommand cmd = new SqlCommand($"INSERT INTO BlogsOf{siteName} (Title, Content,img) VALUES('{blog.Title}', '{blog.Content}',(Select BulkColumn FROM OPENROWSET(BULK N'C:\\Users\\lenovo\\source\\repos\\OuglaWebApp\\OuglaWebApp\\wwwroot\\Default.png', SINGLE_BLOB)  as img));", con);
-            //    cmd.ExecuteNonQuery();
-            //}
-
+            }
             con.Close();
+        }
+        public void upadateAbout(string content, string sitename)
+        {
+            SqlCommand cmd = new SqlCommand($"UPDATE PageContent SET About = '{content}' WHERE sitename = '{sitename}';", con);
+            cmd.ExecuteNonQuery();
+        }
+        public void upadateTwitter(string content, string sitename)
+        {
+            SqlCommand cmd = new SqlCommand($"UPDATE PageContent SET TwitterLink = '{content}' WHERE sitename = '{content}';", con);
+            cmd.ExecuteNonQuery();
+        }
+        public void upadateInsta(string content, string sitename)
+        {
+            SqlCommand cmd = new SqlCommand($"UPDATE PageContent SET InstagramLink = '{content}' WHERE sitename = '{content}';", con);
+            cmd.ExecuteNonQuery();
+        }
+        public void upadateLinkedIn(string content, string sitename)
+        {
+            SqlCommand cmd = new SqlCommand($"UPDATE PageContent SET LinkedInLink = '{content}' WHERE sitename = '{content}", con);
+            cmd.ExecuteNonQuery();
+        }
+        public void upadateThemeColorCode(string content, string sitename)
+        {
+            SqlCommand cmd = new SqlCommand($"UPDATE PageContent SET ThemeColorCode = '{content}' WHERE sitename = '{content}';", con);
+            cmd.ExecuteNonQuery();
+        }
+        public void upadateLogoImg(string content, string sitename)
+        {
+            SqlCommand cmd = new SqlCommand($"UPDATE PageContent SET logoImg = {content} WHERE sitename = '{content}';", con);
+            cmd.ExecuteNonQuery();
+        }
+        public void upadateBanner(string content, string sitename)
+        {
+            SqlCommand cmd = new SqlCommand($"UPDATE PageContent SET BannerImg = {content} WHERE sitename = '{content}';", con);
+            cmd.ExecuteNonQuery();
+        }
+        public void upadateObjImg(string content, string sitename)
+        {
+            SqlCommand cmd = new SqlCommand($"UPDATE PageContent SET ObjectImg = {content} WHERE sitename = '{content}';", con);
+            cmd.ExecuteNonQuery();
         }
     }
 }
